2956ad6d8bc03afde4e595fa56e761f7695d9ac6
BelugaFL5 <begula123765@gmail.com>
2024-08-19 17:37:49 +0800

    register login and admin dashboard has been implemented. However, it requires fine tune to meet requirements. Find ways to implement authentication and authorization by dividing users to different user roles. Implement user class for better system structure.

--- src/system/Main.java
+++ src/system/Main.java
@@ -1,3 +1,5 @@
+package system;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
